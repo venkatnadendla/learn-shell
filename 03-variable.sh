7dd44cd9dcf4ce21315b613158a36cd5d8bc560f
venkat <ramana.nadela@gmail.com>
2023-02-20 23:12:00 +0530

    Arthemetic substitution

--- 03-variable.sh
+++ 03-variable.sh
@@ -4,3 +4,7 @@ echo "Hello, Good Morning, Today date is ${DATE}"
 #Command substitution
 DATE=$(date)
 echo "Hello, Good Morning, Today date is ${DATE}"
+
+#Arthimetic operations
+ADD=$((2+3))
+echo "Addition of 2+3 = $(ADD)"
